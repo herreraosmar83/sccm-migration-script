cfba47da91c9856bd432a7e1e524d57e5de9878f
herreraosmar83 <osmar.herreraperez.945@my.csun.edu>
2020-02-28 10:37:15 -0800

    safe credentials

--- script.py
+++ script.py
@@ -2,9 +2,13 @@ import pyodbc
 import csv
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
-bearer =  os.environ.get('bearer-token')
-connectionString = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=;DATABASE=;UID=;PWD="
+server =  os.environ.get('SERVER')
+database =  os.environ.get('DATABASE')
+uid =  os.environ.get('UID')
+password =  os.environ.get('PWD')
+connectionString = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=%s;DATABASE=%s;UID=%s;PWD=%s"
 query = """
 select dbo.Computer_System_DATA.Manufacturer00,dbo.Computer_System_DATA.Model00,dbo.Computer_System_DATA.Name00,dbo.COMPUTER_SYSTEM_PRODUCT_DATA.IdentifyingNumber00,dbo.System_IP_Address_ARR.IP_Addresses0,dbo.System_MAC_Addres_ARR.MAC_Addresses0,dbo.System_DATA.SMSID0,dbo.System_Enclosure_DATA.chassisTypes00
 FROM dbo.Computer_System_DATA,
@@ -16,7 +20,7 @@ dbo.System_Enclosure_DATA
 where 
 dbo.Computer_System_DATA.MachineID = dbo.COMPUTER_SYSTEM_PRODUCT_DATA.MachineID and dbo.COMPUTER_SYSTEM_PRODUCT_DATA.MachineID = dbo.System_IP_Address_ARR.ItemKey and dbo.System_IP_Address_ARR.ItemKey = dbo.System_MAC_Addres_ARR.ItemKey and dbo.System_MAC_Addres_ARR.ItemKey = dbo.System_DATA.MachineID and  dbo.System_DATA.MachineID = dbo.System_Enclosure_DATA.MachineID
 """
-conn = pyodbc.connect(connectionString)
+conn = pyodbc.connect(connectionString % (server,database,uid,password))
 cursor = conn.cursor()
 cursor.execute(query)
 data = cursor.fetchone()
